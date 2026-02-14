981072b82f39af6b31550d8ed27aab45e93d0d70
Mnimbus <matteo.koponen@gmail.com>
2026-02-14 20:11:04 +0200

    Fixed config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -8,7 +8,7 @@
 #define F_CPU 160000000UL
 
 //--UART
-#define UART_IN				RX
+#define UART_IN				PD0
 #define BAUD_RATE 9600
 
 //--Ultra-pins 
@@ -16,9 +16,9 @@
 #define SONAR_ECHO_PIN 		PB1
 
 //--Motor-pins 
-#define MOTOR_ONE_PIN 		PD5
-#define MOTOR_TWO_PIN		PD6
-#define MOTOR_THREE_PIN		PD7
-#define MOTOR_FOUR_PIN		PD8
+#define MOTOR_ONE_PIN 		PB2
+#define MOTOR_TWO_PIN		PB3
+#define MOTOR_THREE_PIN		PB4
+#define MOTOR_FOUR_PIN		PB5
 
 #endif
