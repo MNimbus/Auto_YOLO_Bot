411be18c70f1a5b7c7406f5c832e888a942ad40e
Mnimbus <matteo.koponen@gmail.com>
2026-02-15 20:24:46 +0200

    Added prototypes for logic.hpp

---
+++ include/logic.hpp
@@ -0,0 +1,13 @@
+#ifndef LOGIC_HPP
+#define LOGIC_HPP
+
+#include "motors.hpp"
+
+void search(Motor& left, Motor& right);
+void moveForward(Motor& left, Motor& right);
+void moveBackward(Motor& left, Motor& right);
+void steerLeft(Motor& left, Motor& right);
+void steerRight(Motor& left, Motor& right);
+void fullStop(Motor& left, Motor& right);
+
+#endif
