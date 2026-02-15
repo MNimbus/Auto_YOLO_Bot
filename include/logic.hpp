6c65ad3128dc26bec71752af65fbc76c0a2041d3
Mnimbus <matteo.koponen@gmail.com>
2026-02-15 22:27:29 +0200

    Added the enum to the logic headerfile

--- include/logic.hpp
+++ include/logic.hpp
@@ -4,6 +4,16 @@
 #include <avr/io.h>
 #include "motors.hpp"
 
+//Define the State machine
+enum RobotState {
+	SEARCHING,
+	TRACK_LEFT,
+	TRACK_CENTER,
+	TRACK_RIGHT,
+	STOP
+};
+
+//Different drive functions
 void search(Motor& left, Motor& right);
 void moveForward(Motor& left, Motor& right);
 void moveBackward(Motor& left, Motor& right);
