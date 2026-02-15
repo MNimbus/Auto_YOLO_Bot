d303c03fd43b330c913ba29b9603607a277f7bff
Mnimbus <matteo.koponen@gmail.com>
2026-02-15 22:26:05 +0200

    Fixed capitalization error in the names of functions

--- src/uart.cpp
+++ src/uart.cpp
@@ -2,7 +2,7 @@
 #include "uart.hpp"
 #include "config.hpp"
 
-void uart_init(uint16_t ubrr) {
+void UART_init(uint16_t ubrr) {
 	//UBRR0H(High) & UBRR0L(Low) baud rate registers
 	UBRR0H = (uint8_t)(ubrr >> 8);
 	UBRR0L = (uint8_t)ubrr;
@@ -12,7 +12,7 @@ void uart_init(uint16_t ubrr) {
 	UCSROC = (1 << UCSZ01) | (1<< UCSZ00); //Sets the frame format 
 }
 
-unsigned char uart_receive(void) {
+unsigned char UART_receive(void) {
 	while (!(UCSR0A & (1 << RXC0)))//Wait for data to be received
 	//^ polling loop,  looks at the RXC0 bit 
 	//if the bit is 0, the CPU stays in this loop because no data has arrived
