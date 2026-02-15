b108bd6ba6ae14aa3038e246ce1703f441dcc252
Mnimbus <matteo.koponen@gmail.com>
2026-02-15 20:27:00 +0200

    wrote the functions for different drive states

---
+++ src/logic.cpp
@@ -0,0 +1,92 @@
+#include "logic.hpp"
+#include <util/delay.h>
+
+void search(Motor& fL, Motor& bL, Motor& fR, Motor& bR) {
+	//Left Speed 
+	fL.setSpeed();
+	bL.setSpeed();
+	//Right Speed 
+	fR.setSpeed();
+	bR.setSpeed();
+		
+	//Left wheels
+	fL.forward();
+	bL.forward();
+	//Right Wheels
+	fR.backward();
+	bR.backward();
+	
+	_delay_ms(100);
+	
+	fullStop(fL, bL, fR, bR);
+	}
+	
+void moveForward(Motor& fL, Motor& bL, Motor& fR, Motor& bR) {
+	//Left Speed 
+	fL.setSpeed();
+	bL.setSpeed();
+	//Right Speed 
+	fR.setSpeed();
+	bR.setSpeed();
+	
+	//Left Wheels
+	fL.forward();
+	bL.forward();
+	//Right Wheels
+	fR.forward();
+	bR.forward();
+}
+	
+void moveBackward(Motor& fL, Motor& bL, Motor& fR, Motor& bR) {
+	//Left Speed 
+	fL.setSpeed();
+	bL.setSpeed();
+	//Right Speed 
+	fR.setSpeed();
+	bR.setSpeed();
+	
+	//Left Wheels
+	fL.backward();
+	bL.backward();
+	//Right Wheels
+	fR.backward();
+	bR.backward();
+	}
+void steerLeft(Motor& fL, Motor& bL, Motor& fR, Motor& bR) {
+	//Left Speed 
+	fL.setSpeed();
+	bL.setSpeed();
+	//Right Speed 
+	fR.setSpeed();
+	bR.setSpeed();
+	
+	//Left Wheels
+	fL.backward();
+	bL.backward();
+	//Right Wheels
+	fR.backward();
+	bR.backward();
+	}
+void steerRight(Motor& fL, Motor& bL, Motor& fR, Motor& bR) {
+	//Left Speed 
+	fL.setSpeed();
+	bL.setSpeed();
+	//Right Speed 
+	fR.setSpeed();
+	bR.setSpeed();
+	
+	//Left Wheels
+	fL.backward();
+	bL.backward();
+	//Right Wheels
+	fR.backward();
+	bR.backward();	
+}
+void fullStop(Motor& fL, Motor& bL, Motor& fR, Motor& bR) {
+	//Left Wheels
+	fL.stop();
+	bL.stop();
+	//Right Wheels
+	fR.stop();
+	bR.stop();
+}
